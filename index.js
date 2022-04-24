ed101222a8ceb844a0eb97ae45f3003ee6355c5d
Itta <86896657+itta611@users.noreply.github.com>
2022-04-24 14:14:24 +0900

    fix channel!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const https = require('https');
 const jsdom = require('jsdom');
 const cron = require('node-cron');
 const atcoderURL = 'https://atcoder.jp/contests/?lang=en';
-const CHANNEL_ID = '958575959714451528';
+const CHANNEL_ID = '967640727142146060';
 const SERVER_ID = '946371427202908180';
 let channel;
 setSchedule();
@@ -28,7 +28,7 @@ function setSchedule() {
         const contestScheduleTime = new Date(
           item.querySelector('time').textContent
         );
-        contestScheduleTime.setHours(contestScheduleTime.getHours() - 1);
+        contestScheduleTime.setMinutes(contestScheduleTime.getMinutes() - 30);
         const contestTitle = item
           .querySelectorAll('td')[1]
           .querySelector('a').textContent;
